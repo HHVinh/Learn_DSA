c92086db66f49d640a2ad51cdef652c2d0709089
HHVinh <vinh0986585015@gmail.com>
2025-10-03 10:34:39 +0700

    Learn Leetcode: Concatenation of Array

---
+++ concatenationOfArray.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        # Tạo ra một list mới = nums nối với chính nó
+        return nums + nums
+
+
+if __name__ == "__main__":
+    # Nhập mảng ban đầu từ bàn phím (các số cách nhau bởi dấu cách)
+    raw_input = input("Nhập mảng ban đầu (các số cách nhau bởi dấu cách): ")
+    
+    # Chuyển chuỗi nhập vào thành list số nguyên
+    nums = list(map(int, raw_input.split()))
+    
+    # Gọi hàm trong class Solution
+    solution = Solution()
+    result = solution.getConcatenation(nums)
+    
+    # In kết quả
+    print("Mảng ban đầu:", nums)
+    print("Mảng sau khi nhân đôi:", result)
