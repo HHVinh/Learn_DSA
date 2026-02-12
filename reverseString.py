11ab1a409b2886b8183fb15339fe8db60355a724
HHVinh <vinh0986585015@gmail.com>
2026-02-12 23:22:00 +0700

    feat: add LeetCode 334 solution - Reverse String

---
+++ reverseString.py
@@ -0,0 +1,22 @@
+# 334. Reverse String
+
+from typing import List
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l += 1
+            r -= 1
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    s = ["h", "e", "l", "l", "o"]
+    solution.reverseString(s)
+    print(s)  # Output: ["o", "l", "l", "e", "h"]
